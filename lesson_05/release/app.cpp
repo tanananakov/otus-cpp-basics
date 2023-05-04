3f8946e8701fb131a5542ec800d4bbe93e339beb
tanananakov <tanankov.art@gmail.com>
2023-05-04 18:49:21 +0300

    input function is working correctly

--- lesson_05/release/app.cpp
+++ lesson_05/release/app.cpp
@@ -6,32 +6,58 @@
 
 
 
-int main(int argc, char  **argv)
+int main(int argc, char** argv) {
 	// argc - count of input arguments
 	// argv - vector with arguments values
 
 	//inargument sort
 	try {
 		if (argc > 1) {
-			for (unsigned int i = 1, i < argc, i++) {
-				switch (argv[i]) {
-				case "-table": // c++11 constexpr
-
+			for (int i = 1; i < argc; i++) {
+				/*switch (argv[i]) {
+				case "-table" : // c++11 constexpr
+					tableFlag++;
+					break;
 				case "-max":
-
+					maxValue = std::stoi(argv[i + 1]);
 				case "-level":
-
-				default:
+					levelFlag = std::stoi(argv[i + 1]);
+				default: // for general purpose
 					continue;
 
+				}*/ // const char is not supported in switchcase
+				std::string dummy{ argv[i] };
+				if (dummy == "-table") {
+					tableFlag = 'A';
+					break;
+				}
+				else if (dummy == "-max") {
+					//std::string dummy2{ argv[i+1] };
+					maxValue = std::stoi(argv[i + 1]);
+				}
+				else if (dummy == "-level") {
+					//std::string dummy2{ argv[i + 1] };
+					levelFlag = std::stoi(argv[i + 1]);
+				}
+				else {
+					continue;
 				}
 			}
 		}
-		else { //default
-			unsigned char tableFlag = 0;
-			unsigned int maxValue = 100;
-			unsigned int levelFlag = 0;
+		if ((maxValue > 0) && (levelFlag > 0)){
+			std::cout << "Entering both maximum value and level is prohibited\n";
+			//return 1;
 		}
-	}catch () {
-
+		if ((maxValue < 0) || (levelFlag < 0)) {
+			std::cout << "Entered value is lower than 0\n";
+			//return 1;
+		}
 	}
+	catch (std::invalid_argument const& exc) {// catch string parsing error
+		std::cout << "Entered value is incorrect\n";
+		std::cout << exc.what() << "\n";
+		//return 1;
+	}
+
+	std::cout << "table " << tableFlag << " max " << maxValue << " level " << levelFlag;
+}
---
+++ lesson_05/release/app.h
@@ -0,0 +1,5 @@
+#pragma once
+
+ char tableFlag = 'B';
+ int maxValue = 0;
+ int levelFlag = 0;
